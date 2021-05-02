73b39f2f3f3f461ad3bfd7743f1aacf386782f46
Paweł Krolik <pawel8krolik@gmail.com>
2021-05-02 17:02:20 +0200

    Add NormalizedOctaveNoise fo 1, 2 and 3 D

--- Source/PGSpherePluginRuntime/Public/PGPerlinNoise.h
+++ Source/PGSpherePluginRuntime/Public/PGPerlinNoise.h
@@ -23,4 +23,12 @@ public:
 	UFUNCTION(BlueprintCallable)
 	float Noise2D(float X, float Y) const noexcept { return PerlinNoise.noise2D(X, Y); }
 
+	UFUNCTION(BlueprintCallable)
+	float NormalizedOctaveNoise1D(float X, int32 Octaves) const noexcept { return PerlinNoise.normalizedOctaveNoise1D(X, Octaves); }
+
+	UFUNCTION(BlueprintCallable)
+	float NormalizedOctaveNoise2D(float X, float Y, int32 Octaves) const noexcept { return PerlinNoise.normalizedOctaveNoise2D(X, Y, Octaves); }
+
+	UFUNCTION(BlueprintCallable)
+	float NormalizedOctaveNoise3D(float X, float Y, float Z, int32 Octaves) const noexcept { return PerlinNoise.normalizedOctaveNoise3D(X, Y, Z, Octaves); }
 };
